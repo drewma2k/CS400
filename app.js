3dd211f7943558ee690b55a6aea9cb6ec3483fb7
Drew Abram <abramd@bu.edu>
2020-04-03 16:03:24 -0400

    add files for ps4

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const ps3Router = require('./routes/ps3');
+const ps4Router = require('./routes/ps4');
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/ps3', ps3Router);
+app.use('/ps4', ps4Router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
